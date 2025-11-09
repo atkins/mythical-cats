bc3035e0ceb3756db12c1ec23eb6af7f6cba6b30
Atkins Meyer <atkinsmeyer@gmail.com>
2025-11-08 22:20:44 -0700

    feat: add ResourceType enum with display names and icons

---
+++ lib/models/resource_type.dart
@@ -0,0 +1,50 @@
+/// Types of resources in the game
+enum ResourceType {
+  cats,
+  offerings,
+  prayers,
+  divineEssence,
+  ambrosia,
+  ichor,
+  celestialFragments;
+
+  /// Display name for UI
+  String get displayName {
+    switch (this) {
+      case ResourceType.cats:
+        return 'Cats';
+      case ResourceType.offerings:
+        return 'Offerings';
+      case ResourceType.prayers:
+        return 'Prayers';
+      case ResourceType.divineEssence:
+        return 'Divine Essence';
+      case ResourceType.ambrosia:
+        return 'Ambrosia';
+      case ResourceType.ichor:
+        return 'Ichor';
+      case ResourceType.celestialFragments:
+        return 'Celestial Fragments';
+    }
+  }
+
+  /// Icon for UI (placeholder, will be replaced with actual icons later)
+  String get icon {
+    switch (this) {
+      case ResourceType.cats:
+        return '🐱';
+      case ResourceType.offerings:
+        return '🎁';
+      case ResourceType.prayers:
+        return '🙏';
+      case ResourceType.divineEssence:
+        return '✨';
+      case ResourceType.ambrosia:
+        return '🍯';
+      case ResourceType.ichor:
+        return '💉';
+      case ResourceType.celestialFragments:
+        return '💎';
+    }
+  }
+}
